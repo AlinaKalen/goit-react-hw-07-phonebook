07852ed528d60b9080324c2d483a8ed3e874db37
AlinaKalen <alina103599@gmail.com>
2024-01-16 17:27:07 +0000

    upd

--- src/components/redux/Operations.js
+++ src/components/redux/Operations.js
@@ -26,7 +26,6 @@ export const addContact = createAsyncThunk(
     }
   }
 );
-
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId, thunkAPI) => {
@@ -34,18 +33,34 @@ export const deleteContact = createAsyncThunk(
       const response = await axios.delete(`/contacts/${contactId}`);
       return response.data;
     } catch (e) {
+      if (e.response && e.response.status === 404) {
+        return thunkAPI.rejectWithValue('Contact not found');
+      }
       return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
-export const deleteContactsThunk = createAsyncThunk(
-  'contacts/deleteContact',
-  async (id, { reject }) => {
-    try {
-      const data = deleteContact(id);
-      return data;
-    } catch (error) {
-      return reject(error.message);
-    }
-  }
-);
+
+
+// export const deleteContact = createAsyncThunk(
+//   'contacts/deleteContact',
+//   async (contactId, thunkAPI) => {
+//     try {
+//       const response = await axios.delete(`/contacts/${contactId}`);
+//       return response.data;
+//     } catch (e) {
+//       return thunkAPI.rejectWithValue(e.message);
+//     }
+//   }
+// );
+// export const deleteContactsThunk = createAsyncThunk(
+//   'contacts/deleteContact',
+//   async (id, { reject }) => {
+//     try {
+//       const data = deleteContact(id);
+//       return data;
+//     } catch (error) {
+//       return reject(error.message);
+//     }
+//   }
+// );
